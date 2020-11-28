997d3120b6033cf0ac61eab4e3584413c4a4021f
Danny <eternal_dante@yahoo.com>
2020-11-28 22:46:12 +0700

    Initial commit

---
+++ xamarin_app/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xamarin_app
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+        async void Click_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            await DisplayAlert("Click!",
+                $"You clicked {count} times.",
+                "OK");
+        }
+
+        async void Reset_Clicked(object sender, System.EventArgs e)
+        {
+            count = 0;
+            await DisplayAlert("Reset!",
+                "You reset clicked",
+                "OK");
+        }
+    }
+}
